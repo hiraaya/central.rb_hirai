6d2d87a4bd0fa4bba10d733de36f99792b8e778b
hirai ayaka <4520-6@auone.jp>
2012-12-16 13:31:13 +0900

    booklist

---
+++ app/booklist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booklist::Application.config.secret_token = '98122f418dc526507410f2a814201fc294cddad1659499b116ae4fb5f430a4a958b40119b88318c4ec00b0abbba98a7815901fb13ceb2b392058d49c0f8bfeeb'
